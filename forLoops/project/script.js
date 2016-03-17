26c0212882f18c227ab9880f7edcd1b5ba7085a2
mamadouovo <mamadousanganaba@gmail.com>
2016-03-17 20:21:31 +0000

    classcode

--- forLoops/project/script.js
+++ forLoops/project/script.js
@@ -1,2 +1,20 @@
 $( document ).ready(function() {
+//     $('body').append('<img src="http://thumbs.dreamstime.com/z/programming-code-blue-tint-4691815.jpg"/>')
+    
+//     var count = 0;
+//   //  var   amount = 0;
+//     while (count < 100) { 
+//       if (count % 2 == 0) {
+//       $('body').append('<img src="http://thumbs.dreamstime.com/z/programming-code-blue-tint-4691815.jpg"/>')  
+//       }
+//     else {
+//         $('body').append('<img src="http://previews.123rf.com/images/kgtoh/kgtoh1101/kgtoh110100705/8635164-Background-concept-wordcloud-illustration-of-source-code-programming-Stock-Illustration.jpg"/>')
+//       }
+//       count += 1;
+//     }
+
+
+for (var count = 0; count < 100; count++) {
+    $('body').append('<img src="http://thumbs.dreamstime.com/z/programming-code-blue-tint-4691815.jpg"/>')
+}
 });
